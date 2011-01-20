625f0a80190db6255d98d477bbf29f4a33543a29
Austin King <shout@ozten.com>
2011-01-19 20:08:51 -0800

    commit
    First cut at a db migration list

--- js/main.js
+++ js/main.js
@@ -1,5 +1,78 @@
 /* Firebug is crashy on stale IndexedDB Objects(???), only log primitives  or use Minefield and WebConsole */
+
 $(document).ready(function(){
+    var dbName = "IdeaCatcherDBv13";
+    var objectStoreName = "ideas";
+
+    var handleError = function(event) {
+        // Do something with request.errorCode!
+        console.info("ERROR handler");
+        console.info("ERROR: #", event.target.errorCode);
+    };
+    var setupDb = function (event) {
+        var req = window.indexedDB.open(dbName, "All your ideas are belong to us."),
+            migrations = [
+                0, 
+                function (req) {
+                    /* A fn that make a onsuccess handler, which captures the db from the request */
+                    console.info("Manufacturing fn which closes over ", req);
+                    return function(event) {
+                        var trans = req.result;
+                        console.info("Creating object store", objectStoreName, "having opened", dbName, " db=", trans.db);
+                        var db = trans.db;
+                        objectStore = db.createObjectStore(objectStoreName, {keyPath: 'id', autoIncrement: true}, false);
+                        objectStore.createIndex("title", "id", { unique: false });
+                        console.info("Finished Creating object store");
+                        migrate(event);//bootstrap next iteration
+                    }
+                },
+                function (req) {
+                    console.info("Manufacturing 2nd fn which closes over ", req);
+                    return function (event) {
+                        var trans = req.result;
+                        var db = trans.db;
+                        console.info("writing data to db=", db);
+                        writeData(db);
+                    }
+                },
+            ],
+            migrate = function (event) {
+                // .source IDBFactory, .result IDBDatabase, req.LOADING, req.DONE, req.readyState
+                dbVersion = parseInt(req.result.version);
+                dbVersion = isNaN(dbVersion) ? 0 : dbVersion;
+                console.info("DB Version is ", dbVersion, " we know about ", migrations.length - 1 );
+                if ((dbVersion + 1) < migrations.length) {
+                    setVerReq = req.result.setVersion(dbVersion + 1);
+                    req.result.onerror = handleError;
+                    setVerReq.onsuccess = (migrations[dbVersion + 1])(setVerReq);
+                    setVerReq.onerror = handleError;
+                    
+                }
+            },
+        setVerReq, dbVersion;
+        req.onerror = handleError;
+        req.onsuccess = migrate;
+
+    };//end setupDb
+    var writeData = function (db) {
+        var transaction = db.transaction([objectStoreName], IDBTransaction.READ_WRITE);
+        var addReq;
+        transaction.onerror = handleError;
+        // Do something when all the data is added to the database.
+        transaction.oncomplete = function(event) {
+            console.info("All done writing data!");
+        };
+
+        var objectStore = transaction.objectStore(objectStoreName);
+        for (var i in firstIdeas) {
+            addReq = objectStore.add(firstIdeas[i]);
+            addReq.onsuccess = function(event) {
+                // event.target.result == firstIdeas[i].ssn
+                console.info("Done writing ", event.target.result);
+            };
+        }
+    }; //end function writeData 
+
     $('nav ul li').text('Ok');
 
     if ("webkitIndexedDB" in window) {
@@ -12,75 +85,27 @@ $(document).ready(function(){
         alert("You're browser aint gonna work.");
     }
 
-// This is what our customer data looks like.
-var firstIdeas = [
+    var firstIdeas = [
     { 
         /*id: 10,  */
       title: 'Welcome to Idea Catcher',
       content: 'Idea Catcher is a quick notebook for ideas and TODOs'
       }
-];
+    ];
 
-    var handleError = function(event) {
-        // Do something with request.errorCode!
-        console.info("ERROR handler");
-        console.info("ERROR: #", event.target.errorCode);
-        console.info("ERROR: target=", event.code);
-    };
+    
     var db;
-    var dbName = "IdeaCatcherDBv20";
-    var objectStoreName = "ideas";
+
     if (window.indexedDB) {
-        window.request = window.indexedDB.open(dbName, "All your ideas are belong to us.");
-        window.request.onerror = handleError;
+        setupDb();
 
-    var initDb = function(db) {
-        console.warn("No version, creating one");
-            var request2 = db.setVersion('1');
-            request2.onerror = handleError;
-            request2.onsuccess = function(event) {
-                objectStore = db.createObjectStore(objectStoreName, {keyPath: 'id', autoIncrement: true}, false);
-            /* 
-               {keyPath: 'id'}, true - value objects must have an id property 
-               {keyPath: 'id', autoIncrement: true}, true - value object shouldn't have an id property, if it does, then the value object's id overrides the generator
-*/
 
-                //autoIncrement: true - IndexedDB handles key generation
-                objectStore.createIndex("title", "id", { unique: false });
-                console.info("Object store created", objectStore);
-            }
-    };
-    var writeData = function (db) {
-        var transaction = db.transaction([objectStoreName], IDBTransaction.READ_WRITE);
-            // Do something when all the data is added to the database.
-            transaction.oncomplete = function(event) {
-              console.info("All done writing data!");
-            };
-
-            transaction.onerror = handleError;
-
-        var objectStore = transaction.objectStore(objectStoreName);
-        for (var i in firstIdeas) {
-          var request = objectStore.add(firstIdeas[i]);
-          request.onsuccess = function(event) {
-            // event.target.result == firstIdeas[i].ssn
-              console.info("Done writing ", event.target.result);
-          };
-        }
-    }; //end function writeData 
-    var getOneRecord = function (db, id) {
-        var transaction = db.transaction([objectStoreName]);
-        var objectStore = transaction.objectStore(objectStoreName);
-        window.request = objectStore.get(id);
-        window.request.onerror = handleError;
-        window.request.onsuccess = function(event) {
-          // Do something with the request.result!
-          console.info("Name for ", id, " is " + window.request.result.title);
-console.info("Name for ", id, " is " + window.request.result.content);
-        };
-    }; //end function getOneRecord
-
+        setTimeout(function () {
+            window.readOpenRequest = window.indexedDB.open(dbName, "All your ideas are belong to us.");
+            window.readOpenRequest.onerror = handleError;
+        
     var getAllData = function (db) {
+        console.info("Reading ", objectStoreName);
         var transaction = db.transaction([objectStoreName]);
         var objectStore = transaction.objectStore(objectStoreName);
         var request3 = objectStore.getAll();
@@ -96,6 +121,59 @@ console.info("Name for ", id, " is " + window.request.result.content);
             }
         };
     }; //end function getAllData
+
+
+    var handleDbOpen = function (event) {
+        var db = window.readOpenRequest.result;
+        console.info("size of object stores: ", db.objectStoreNames.length);
+        for (var i=0; i < db.objectStoreNames.length; i++) {
+            console.info("OBJECT STORE: ", db.objectStoreNames[i]);
+        }
+        for (var l in db) {
+            //            console.info("hmm ", l, "=", db[l]);
+        }
+        db.onerror = handleError; //TODO remove other error handlers
+
+                   getAllData(db);
+        
+               }; //end handleDbOpen
+               window.readOpenRequest.onsuccess = handleDbOpen;
+            }, 3000);//end setTimeout
+    } // if window.indexedDB
+});//end document ready
+
+            /* 
+    var initDb = function(db) {
+        console.warn("No version, creating one");
+            var request2 = db.setVersion('1');
+            request2.onerror = handleError;
+            request2.onsuccess = function(event) {
+                objectStore = db.createObjectStore(objectStoreName, {keyPath: 'id', autoIncrement: true}, false);
+
+               //{keyPath: 'id'}, true - value objects must have an id property 
+               //{keyPath: 'id', autoIncrement: true}, true - value object shouldn't have an id property, if it does, then the value object's id overrides the generator
+
+
+                //autoIncrement: true - IndexedDB handles key generation
+                objectStore.createIndex("title", "id", { unique: false });
+                console.info("Object store created", objectStore);
+            }
+    };
+*/
+
+/*
+    var getOneRecord = function (db, id) {
+        var transaction = db.transaction([objectStoreName]);
+        var objectStore = transaction.objectStore(objectStoreName);
+        window.request = objectStore.get(id);
+        window.request.onerror = handleError;
+        window.request.onsuccess = function(event) {
+          // Do something with the request.result!
+          console.info("Name for ", id, " is " + window.request.result.title);
+console.info("Name for ", id, " is " + window.request.result.content);
+        };
+    }; //end function getOneRecord
+        */
 
     var deleteData = function (db) {
 //delete
@@ -114,44 +192,3 @@ console.info("Name for ", id, " is " + window.request.result.content);
             console.info("Delete 555-55-5559, It's gone!");
         };
     }; //end function deleteData
-    var handleDbOpen = function (event) {
-        db = window.request.result;
-
-
-        console.info('initial db.version =', db.version);
-        if (db.version === undefined ||
-            db.version === '') {
-            initDb(db);
-        } else {
-            console.info('db.version=', db.version);
-            var r4 = db.setVersion("5");
-            r4.onsuccess = function () {
-                  console.info("Taking it to two");
-                  console.info('db.version=', db.version);
-                  r5 = db.deleteObjectStore(objectStoreName);
-                  console.info("Boo ya");
-                  r5.onsuccess = function () {
-                      console.info("inner Taking it to two");
-                      console.info('inner db.version=', db.version);
-                      r5 = db.deleteObjectStore(objectStoreName);
-                      console.info("inner Boo ya");
-                  };
-            };
-
-        //Hmm request doesn't see good yet...
-
-                db.onerror = handleError; //TODO remove other error handlers
-
-                writeData(db);
-                getAllData(db);
-        
-                //getOneRecord(db, 10);
-        
-                //deleteData(db);
-            }
-        }; //end handleDbOpen
-        window.request.onsuccess = function(event) {
-            handleDbOpen(event);
-        } //end open db
-    } // if window.indexedDB
-});//end document ready
