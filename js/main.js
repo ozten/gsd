698372ff96d14bcc9f41f332c4b6d2efbe906ede
Austin King <shout@ozten.com>
2011-01-31 21:31:27 -0800

    Adding contexts to next actions

--- js/main.js
+++ js/main.js
@@ -4,8 +4,12 @@ $(document).ready(function(){
     var dbName = "IdeaCatcherDBv13";
     var dbDescription = "All your ideas are belong to us.";
     var objectStoreName = "ideas";
+    var contextOSName = "contexts";
 
-    var currentNextAction = null;
+    var currentNextAction = null; /* TODO this is actually bad, umhkay. Fresh dbName to repro crash */
+
+    var currentContext = "@?";
+    var usedContexts = [];
 
     var handleError = function(event) {
         // Do something with request.errorCode!
@@ -25,19 +29,36 @@ $(document).ready(function(){
                         objectStore.createIndex("title", "id", { unique: false });
                         migrate(event);//bootstrap next iteration
                     }
-                },
+                },/* end migration 1 */
                 function (req) {
                     return function (event) {
                         var trans = req.result;
                         var db = trans.db;
                         writeData(db);
+                        migrate(event);//bootstrap next iteration
                     }
-                },
+                },/* end migration 2 */
+                function (req) {
+                    return function (event) {
+                        var trans = req.result;
+                        var db = trans.db;
+
+                        var objectStore = db.createObjectStore(contextOSName, {keyPath: 'id', autoIncrement: true}, false);
+                        objectStore.createIndex("name", "id", { unique: true });
+                        objectStore.add({name:"Work"});
+                        objectStore.add({name:"Home"});
+                        objectStore.add({name:"Phone"});
+                        objectStore.add({name:"Errands"});
+                        objectStore.add({name:"Grocery Store"});
+                        migrate(event);//bootstrap next iteration
+                    }
+                },/* end migration 3 */
             ],
             migrate = function (event) {
                 // .source IDBFactory, .result IDBDatabase, req.LOADING, req.DONE, req.readyState
                 dbVersion = parseInt(req.result.version);
                 dbVersion = isNaN(dbVersion) ? 0 : dbVersion;
+                console.info("Database version:", dbVersion);
                 if ((dbVersion + 1) < migrations.length) {
                     setVerReq = req.result.setVersion(dbVersion + 1);
                     req.result.onerror = handleError;
@@ -62,10 +83,12 @@ $(document).ready(function(){
 
         var objectStore = transaction.objectStore(objectStoreName);
         for (var i in initialNextActions) {
+            currentNextAction = initialNextActions[i];
             addReq = objectStore.add(initialNextActions[i]);
             addReq.onsuccess = function(event) {
                 // event.target.result == initialNextActions[i].ssn
                 console.info("Done writing ", event.target.result);
+                currentNextAction = event.target.result;
             };
         }
     }; //end function writeData 
@@ -96,7 +119,17 @@ $(document).ready(function(){
         };
         return next_action;
     } // end createNextAction
-    var updateNextAction = function (id, next_action) {
+    var nextActionIDFromDOM = function (domID) {
+        if ('string' == typeof domID && domID.length > 2) {
+            return domID.substring(2);
+        } else if ('number' == typeof domID) {
+            return domID;
+        } else {
+            console.info("ASSERTION FAILED... nextActionIDFromDOM given ", domID);
+        }            
+    };// end nextActionIDFromDOM
+    var updateNextAction = function (domID, next_action) {
+        var id = nextActionIDFromDOM(domID);
         var openReq = window.indexedDB.open(dbName, dbDescription);
         openReq.onerror = handleError;
         openReq.onsuccess = function (event) {
@@ -119,7 +152,9 @@ $(document).ready(function(){
 
         };
     }; //end updateNextAction
-    var getNextAction = function (id, fn) {
+    var getNextAction = function (domID, fn) {
+        var id = nextActionIDFromDOM(domID);
+        console.info("getNextAction (", id, ")");
         var openReq = window.indexedDB.open(dbName, dbDescription);
         openReq.onerror = handleError;
         openReq.onsuccess = function (event) {
@@ -133,7 +168,8 @@ $(document).ready(function(){
             };
         };
     }; //end function getNextAction
-    var deleteNextAction = function (id, successFn) {
+    var deleteNextAction = function (domID, successFn) {
+        var id = nextActionIDFromDOM(domID);
         var openReq = window.indexedDB.open(dbName, dbDescription);
         openReq.onerror = handleError;
         openReq.onsuccess = function (event) {
@@ -179,6 +215,32 @@ $(document).ready(function(){
         };
     }; // end getAllNextActions
 
+/**
+     * loadFn is a function that takes to parameters key and value. It 
+     * returns a boolean - true to continue reading from the DB.
+     * finFn is called when no more data is available.
+     */
+    var getAllContexts = function (loadFn, finFn) {
+        /* TODO aok if this really works this way, abstract */
+        var openReq = window.indexedDB.open(dbName, dbDescription);
+        openReq.onerror = handleError;
+        openReq.onsuccess = function (event) {
+            var db = event.target.result;
+            var trans = db.transaction([contextOSName]);
+            var objectStore = trans.objectStore(contextOSName);
+            objectStore.openCursor().onsuccess = function (cursorEvent) {
+                var cursor = cursorEvent.target.result;
+                if (cursor) {
+                    if (loadFn(cursor.key, cursor.value)) {
+                        cursor.continue();
+                    }
+                } else {
+                    finFn();
+                }
+            };
+        };
+    }; // end getAllContexts
+
     var exportDatabase = function () {
         var actions = [];
         getAllNextActions(            
@@ -191,14 +253,44 @@ $(document).ready(function(){
 
             });
     }
-
+    var idify = function (context) {
+        if (context) {
+            return context.replace(' ', '_');
+        } else {
+            return 'unknown-context';
+        }
+    };// end idify
+    var contextUlSelector = function (next_action) {
+        if (next_action.context) {            
+            //ensure
+            console.info("calling idify 1 [", next_action.context, "]");
+            var nac = idify(next_action.context);
+            console.info("nac is ", nac, " ", $('li#' + nac).size());
+            if ($('li#' + nac).size() == 0) {
+                // aok nac isn't "safe" id="Grocery Store"...
+                // Should we use context.id? or make them safe? boo.
+                $('#contexts > ul').append("<li id='" + nac + "' class='context'><div>@" + nac + "</div><ul></ul></li>");
+            }
+            contextUl = 'li#' + nac + ' ul';
+        } else {
+            contextUl = 'li#unknown-context ul'
+        }
+        return contextUl;
+    };//end contextUlSelector
     var showNewNextAction = function () {
-        var next_action = createNextAction(function () {
+        var next_action = createNextAction(function (next_action) {
             currentNextAction = next_action;
-            console.info("New next action is id=", next_action.id);
-            $('nav ul li.current').removeClass('current');
-            $('nav ul').append("<li id='" + next_action.id + "' class='current'>" + 
+            console.info("New next action is id=na", next_action.id);
+            $('li.next-action.current').removeClass('current');
+            var contextUl = contextUlSelector(next_action);
+            
+            $(contextUl).append("<li id=na'" + next_action.id + "' class='next-action current'>" + 
                                next_action.title + "</li>");
+            if (next_action.context) {
+                $('#context-selector').val(next_action.context);                
+            } else {
+                $('#context-selector').val(0);
+            }
             $('#display textarea').val(next_action.content);
             $('#display textarea').focus();
         });
@@ -207,18 +299,29 @@ $(document).ready(function(){
 
     var showNextAction = function (na_li) {
         $('#display textarea').attr('disabled', 'disabled');
-        console.info("I clicked ", $(na_li).attr('id'));
+        var id = $(na_li).attr('id');
+        console.info("I clicked ", id);
             $(na_li).addClass('current');
             getNextAction($(na_li).attr('id'), function (next_action) {
-                currentNextAction = next_action;
-                $('#display textarea').val(next_action.content);
-                $('#display textarea').attr('disabled', null)
-                    .focus();
+                    if (next_action) {
+                        console.info("Saving next_action as current", next_action);
+                        currentNextAction = next_action;
+                        if (next_action.context) {
+                            $('#context-selector').val(next_action.context);
+                        } else {
+                            $('#context-selector').val(0);
+                        }
+                             $('#display textarea').val(next_action.content);
+                        $('#display textarea').attr('disabled', null)
+                            .focus();
+                    } else {
+                        console.info("WARNING getNextAction failed... called back with ", next_action, " expected id=", id);
+                    }
             });
     };//end showNextAction
 
     var deleteCurrent = function () {
-        var oldLi = $('li.current');
+        var oldLi = $('li.next-action.current');
         oldLi.removeClass('current');
         var id = oldLi.attr('id');
         deleteNextAction(id, function (event) {
@@ -226,7 +329,6 @@ $(document).ready(function(){
             oldLi.remove();
         });
     };//end deleteCurrent
-    $('nav ul li').text('Ok');
 
     if ("webkitIndexedDB" in window) {
         window.indexedDB = window.webkitIndexedDB;
@@ -260,20 +362,32 @@ $(document).ready(function(){
         }
         timeoutId = setTimeout(function () {
                 currentNextAction.content = $('#display textarea').val();
+                currentNextAction.context = $('context-selector').val();
                 currentNextAction.title = currentNextAction.content.split('\n')[0];
-                $('nav ul li.current').text(currentNextAction.title);
+                $('nav ul li.next-action.current').text(currentNextAction.title);
                 timeoutId = null;
-                updateNextAction(currentNextAction['id'], currentNextAction);
+                // Use custom events to decouple
+                updateNextAction(currentNextAction.id, currentNextAction);
             }, 300);
-    }
+    };
     var initUI = function () {
+        $('#context-selector').bind('change', function () {
+            currentNextAction.context = $('#context-selector').val();
+            console.info("changed new value=", currentNextAction.context);
+            if ("0" == currentNextAction.context) {
+                currentNextAction.context = 'unknown-context';
+            }
+            console.info("normaled to new value=", currentNextAction.context);
+            updateNextAction(currentNextAction['id'], currentNextAction);
+            moveNALIContext();
+        });
         $('#display textarea').bind('change, keyup', function (event) {
                 //console.info("Make that change");
             saveCurrent();
         });
         $('#delete-next-action').bind('click', function (event) {
-            var next = $('li.current').next();
-            var prev = $('li.current').prev();
+            var next = $('li.next-action.current').next();
+            var prev = $('li.next-action.current').prev();
             console.info("NEXT = ", next.attr('id'), " ", next.text(),
                          " PREV = ", prev.attr('id'), " ", prev.text());
             deleteCurrent();
@@ -292,35 +406,78 @@ $(document).ready(function(){
         $('button#export_db').bind('click', function (event) {
             exportDatabase();
         });
-        $('li').live('click', function (event) {
-            $('nav ul li.current').removeClass('current');
+        $('li.next-action').live('click', function (event) {
+            $('nav ul li.next-action.current').removeClass('current');
             showNextAction(event.target);
         });
     };// end initUI
+    var moveNALIContext = function () {
+        // nav ul li.context for context must already exist!
+        // nav ul li.context ul must also already exist!
+        var cna = currentNextAction;
+        if (cna.context) {        
+            console.info("calling idify 2 ", 'li#' + idify(cna.context) + '.context ul li#na' + cna.id);
+            if ($('li#' + idify(cna.context) + '.context ul li#na' + cna.id).size() > 0) {
+                // No Op
+                console.info("It's in there");
+            } else {
+                // Move it 
+                console.info("calling idify 3");
+                console.info(cna.id);
+                console.info("Moving ", 'li#na' + cna.id, " (" + $('li#na' + cna.id).size() + ") into ",
+                             'li#' + idify(cna.context) + '.context ul', " (" + $(idify(cna.context) + '.context ul').size() +")");
+                console.info("calling idify 4");
+                $('li#' + idify(cna.context) + '.context ul').append(
+                                                                     $('li#na' + cna.id).remove());
+
+            }
+        } else {
+            console.info("ASSERTION FAILED cna.context undefined, but moveNALIContext called [", cna.context, "]");
+        }
+    }//end moveNALIContext
     setTimeout(function () {    
-            $('nav ul li').remove();
+            $('li.next-action').remove();
             initUI();
+            $('li#unknown-context').append("<ul></ul>");
             getAllNextActions(
                 function (key, value) {
-                    console.info("getAllNextActions callback");
                     var s = "";
                     for (var e in value) {
                         s += e + " " + value[e] + " ";
                     }
-                    $('nav ul').append("<li id='" + key + "'>" + value.title + "</li>");
+                    var contextUl = contextUlSelector(value);
+                    console.info("contextUl = ", contextUl);
+                    $(contextUl).append("<li class='next-action' id='na" + key + "'>" + value.title + "</li>");
                     if ($('#display').hasClass('loading')) {
                         var d = $('#display');
                         d.removeClass('loading');
                         $('span', d).hide();
                         $('textarea').val(value.content);
-                        $('nav ul li:last-child').addClass('current');
+                        if (value.context) {
+                            $('#context-selector').val(value.context);
+
+                        } else {
+                            $('#context-selector').val(0);
+                        }
+                        $('nav ul li.next-action:last-child').addClass('current');
                         currentNextAction = value;
                     }
                     return true;
                 }, 
                 function () {
                     //console.info("Done");
-                }); 
+                    $('#loading-next-actions').remove();
+                });
+            getAllContexts(
+                function (key, value) {
+                    console.info("Loading Context ", key, " ", value.name);
+                    usedContexts.push(value);
+                    $('#context-selector').append("<option value='" + value.name + "'>" + value.name + "</option>");
+                    return true;
+                }, 
+                function () {
+                    console.info("Done loading contexts");
+                });
         }, 100);
     
 });//end document ready
